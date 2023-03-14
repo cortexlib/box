f71260214d0b4a908df1bbf4139dca397cdb5c97
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-03-14 13:41:54 +1100

    Cleaned up match expression syntax sugar. Uses `<<` and `>>` now. Renamed the bottom match type to `match_any` Put `match_any` in `cxl::utils` namespace

--- include/utils/match.hxx
+++ include/utils/match.hxx
@@ -13,7 +13,7 @@
 ///
 /// License: MIT
 ///
-/// Copyright: Copyright (c) 2022
+/// Copyright: Copyright (c) 2022-23
 /// \file match.hxx
 
 #ifndef CORTEX_MATCH
@@ -26,111 +26,109 @@
 #    include <concepts>
 #endif  /// __cpp_concepts >= 201907L
 
-namespace cxl
+namespace cxl::utils
 {
-    namespace utils
+
+    /// \brief match structure
+    ///
+    /// \details Inherits the properties
+    /// of the variadic template of
+    /// invocables and interfaces the
+    /// invocables `()` operators.
+    ///
+    /// \tparam ...Fs
+    template<typename... Fs>
+    struct match : Fs...
     {
-        /// \brief match structure
+
+        /// \brief Forward Constructor
         ///
-        /// \details Inherits the properties
-        /// of the variadic template of
-        /// invocables and interfaces the
-        /// invocables `()` operators.
-        ///
-        /// \tparam ...Fs concept: std::invocable
-        template<typename... Fs>
-        struct match : Fs...
-        {
-
-            /// \brief Forward Constructor
-            ///
-            /// \details Consumes all function types
-            /// and forwards them to the variadic constructor
-            /// of the invocables.
-            ///
-            /// \tparam ...Ts
-            ///
-            /// \param ...ts
-            template<class... Ts>
-            explicit match(Ts&&... ts)
-                : Fs { std::forward<Ts>(ts) }... { }
-
-            /// \brief Invocation declaration
-            ///
-            /// \details Declares `match` to be using
-            /// the invocation operators `()`.
-            using Fs::operator()...;
-        };
-
-        /// \brief Template Deduction Guide
-        template<typename... Fs>
-        match(Fs&&...) -> match<std::remove_reference_t<Fs>...>;
-
-        /// \brief Match Expression Notation
-        ///
-        /// \details Overloads `>>` to reverse the argument
-        /// order of a match expression for a cleaner syntax
-        /// with existing match types.
+        /// \details Consumes all function types
+        /// and forwards them to the variadic constructor
+        /// of the invocables.
         ///
         /// \tparam ...Ts
-        /// \tparam ...Fs concept: std::invocable
-        /// \param var type: std::variant<Ts...>
-        /// \param m type match<Fs...>&&
-        /// \return decltype(auto)
-        template<typename... Ts, typename... Fs>
-        constexpr auto
-        operator| (const std::variant<Ts...>& var,
-                   const match<Fs...>& m) -> void {
-            std::visit(m, var);
-        }
-
-        /// \brief Match Expression Assignment Overload
         ///
-        /// \details Overloads the `>>=` operator to
-        /// reverse the argument order of a match
-        /// expression for a cleaner syntax with existing
-        /// match types and to denote an explicit syntax
-        /// for match expressions that return a value.
-        ///
-        /// \tparam R
-        /// \tparam ...Ts
-        /// \tparam ...Fs concept: std::invocable
-        /// \param var type: const std::variant<Ts...>&
-        /// \param m type: match<Fs...>&&
-        /// \return R
-        template<typename... Ts, typename... Fs>
-        constexpr auto
-        operator|= (const std::variant<Ts...>& var,
-                    const match<Fs...>& m) -> decltype(auto) {
-            return std::visit(m, var);
-        }
+        /// \param ...ts
+        template<class... Ts>
+        explicit match(Ts&&... ts)
+            : Fs { std::forward<Ts>(ts) }... { }
 
-    }  // namespace utils
+        /// \brief Invocation declaration
+        ///
+        /// \details Declares `match` to be using
+        /// the invocation operators `()`.
+        using Fs::operator()...;
+    };
+
+    /// \brief Template Deduction Guide
+    template<typename... Fs>
+    match(Fs&&...) -> match<std::remove_reference_t<Fs>...>;
+
+    /// \brief Match Expression Notation
+    ///
+    /// \details Overloads `>>` to reverse the argument
+    /// order of a match expression for a cleaner syntax
+    /// with existing match types.
+    ///
+    /// \tparam ...Ts
+    /// \tparam ...Fs
+    /// \param var type: const std::variant<Ts...>&
+    /// \param m type const match<Fs...>&
+    /// \return auto
+    template<typename... Ts, typename... Fs>
+    constexpr auto
+    operator>> (
+        const std::variant<Ts...>& var,
+        const match<Fs...>& m
+    ) -> void
+    { std::visit(m, var); }
+
+    /// \brief Match Expression Assignment Overload
+    ///
+    /// \details Overloads the `>>=` operator to
+    /// reverse the argument order of a match
+    /// expression for a cleaner syntax with existing
+    /// match types and to denote an explicit syntax
+    /// for match expressions that return a value.
+    ///
+    /// \tparam ...Ts
+    /// \tparam ...Fs
+    /// \param var type: const std::variant<Ts...>&
+    /// \param m type: const match<Fs...>&
+    /// \return decltype(auto)
+    template<typename... Ts, typename... Fs>
+    constexpr auto
+    operator<< (
+        const std::variant<Ts...>& var,
+        const match<Fs...>& m
+    ) -> decltype(auto) 
+    { return std::visit(m, var); }
 
     /// \brief Wildcard Empty Placeholder
     ///
     /// \details Names a placeholder type
     /// that can match to anything in a match
     /// expression. The value is un-retrievable
-    /// from `_`, use `auto&& <name>` to have a
+    /// from `bottom`, use `auto&& <var-name>` to have a
     /// retrievable value.
-    struct _
+    struct match_any
     {
         template<typename T>
-        explicit constexpr _([[maybe_unused]] T&& t) noexcept { }
+        explicit constexpr match_any([[maybe_unused]] T&& t) noexcept { }
 
-        explicit constexpr _(const _&) noexcept = delete;
-        explicit constexpr _(_&&) noexcept      = delete;
-
-        constexpr auto
-        operator= (const _&) noexcept -> _& = delete;
+        explicit constexpr match_any(const match_any&) noexcept = delete;
+        explicit constexpr match_any(match_any&&) noexcept      = delete;
 
         constexpr auto
-        operator= (_&&) noexcept -> _& = delete;
+        operator= (const match_any&) noexcept -> match_any& = delete;
 
-        constexpr ~_() noexcept = delete;
+        constexpr auto
+        operator= (match_any&&) noexcept -> match_any& = delete;
+
+        constexpr ~match_any() noexcept = delete;
     };
 
-}  // namespace cxl
+}  // namespace cxl::utils
 
 #endif  /// CORTEX_MATCH
